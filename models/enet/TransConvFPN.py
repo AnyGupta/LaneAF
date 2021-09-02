b544d95cb9c3d4cfaa24861f8189e3eb984c392d
Annie <any2027@gmail.com>
2021-09-02 16:07:48 +0530

    Update TransConvFPN.py

--- models/enet/TransConvFPN.py
+++ models/enet/TransConvFPN.py
@@ -2,4 +2,7 @@ import torch
 import torch.nn as nn
 
 class TransConvFPN(nn.Module):
-    def __init__(self, in_channels, out_channels, num_outs)
+    def __init__(self, in_channels, out_channels, num_outs):
+        
+        super().__init__()
+        
